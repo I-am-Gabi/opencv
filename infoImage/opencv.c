8ce7ebd0aea3c27c8429f5c2130b381246e28545
I-am-Gabi <gabicavalcantesilva@gmail.com>
2015-09-27 02:17:42 +0200

    data image

---
+++ infoImage/opencv.c
@@ -0,0 +1,30 @@
+#include "cv.h"
+#include "highgui.h" 
+#include <stdio.h>
+
+/*
+	
+*/
+void getDataImage(IplImage* img) {   
+
+	int height     = img->height;
+	int width      = img->width;
+	int step       = img->widthStep/sizeof(uchar);
+	int channels   = img->nChannels; 
+	int depth 	   = img->depth;
+	int size 	   = img->imageSize;  
+ 
+	printf("Wigth %d\n", width);  
+	printf("Height %d\n", height);  
+	printf("Pixel Depth %d\n", depth);  
+	printf("Channels %d\n", channels);  
+	printf("Wight Step %d\n", step);  
+	printf("Image Size %d\n", size); 
+}
+
+int main(int argc, char** argv)
+{ 
+	IplImage* image = cvLoadImage("Example1.jpeg", 1); 
+	getDataImage(image);
+	return 0;
+}
